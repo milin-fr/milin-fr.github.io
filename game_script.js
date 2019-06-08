cf01935e8ba41ecdc496d80976d4fcf62778c6d6
Mikhail Ilin <m.ilin.fr@gmail.com>
2019-06-09 00:13:24 +0200

    added win text display

--- game_script.js
+++ game_script.js
@@ -6,7 +6,7 @@ var current_turn = 1;
 var current_cell_pair = [];
 var guessed_cells = [];
 var number_of_guessed_cells = 0;
-
+var game_won = "nope";
 
 function clear_board() {
     images = ["./game_images/ant.png", "./game_images/bug.png", "./game_images/cat.png", "./game_images/cerf.png", "./game_images/chiken.png", "./game_images/cow.png", "./game_images/crab.png", "./game_images/dauphin.png", "./game_images/dog.png", "./game_images/donkey.png", "./game_images/elephant.png", "./game_images/frog.png", "./game_images/hedgehog.png", "./game_images/horse.png", "./game_images/octopus.png", "./game_images/pig.png", "./game_images/rabbit.png", "./game_images/sheep.png", "./game_images/turtle.png"];
@@ -14,8 +14,10 @@ function clear_board() {
     current_cell_pair = [];
     guessed_cells = [];
     number_of_guessed_cells = 0;
+    game_won = "nope";
     get_eight_random_couples_of_images();
     get_image_per_cell_dictionary();
+    display_game_won();
     for (var i = 0; i < all_cells.length; i++) {
         all_cells[i].src = "./game_images/empty_green.png"
     }
@@ -60,7 +62,10 @@ var restart_game = document.querySelector("#restart_button")
 restart_game.addEventListener('click',clear_board);
 
 function check_winning_conditions() {
-    console.log("lol")
+    if (number_of_guessed_cells == 16){
+        game_won = "yes";
+        display_game_won();
+    }
 }
 
 function close_not_matching_cells() {
@@ -76,6 +81,8 @@ function check_for_match() {
     if(current_cell_pair[0].src == current_cell_pair[1].src){
         guessed_cells.push(current_cell_pair[0]);
         guessed_cells.push(current_cell_pair[1]);
+        number_of_guessed_cells = number_of_guessed_cells + 2;
+        check_winning_conditions();
     }
 }
 
@@ -97,8 +104,19 @@ function cell_clicked() {
     }
 }
 
+
+function display_game_won(){
+    if(game_won == "yes") {
+        document.getElementById('game_won').style.display = 'block';
+        document.getElementById("number_of_turns").innerHTML = current_turn;
+    }else {
+        document.getElementById('game_won').style.display = 'none';
+    }
+}
+
 for (var i = 0; i < all_cells.length; i++) {
     all_cells[i].addEventListener('click',cell_clicked);
 }
+
 
 clear_board();
